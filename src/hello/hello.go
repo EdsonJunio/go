09bcd51552b74acbd88bc6d1532a0c29d6d12e1a
EdsonJunio <edsonbh97@gmail.com>
2024-08-21 21:33:41 -0300

    go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -17,6 +17,7 @@ const delay = 5
 
 func main() {
 	exibeIntroducao()
+	registraLog("site", false)
 
 	for {
 		exibeMenu()
@@ -120,24 +121,22 @@ func leSitesDoArquivo() []string {
 }
 
 func registraLog(site string, status bool) {
-
 	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Ocorreu um erro:", err)
 	}
 
-	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
 
 func imprimeLogs() {
-
 	arquivo, err := ioutil.ReadFile("log.txt")
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Ocorreu um erro:", err)
 	}
 
 	fmt.Println(string(arquivo))
